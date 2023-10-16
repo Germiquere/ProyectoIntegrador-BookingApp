4c428ead5a30ab8d3d89940d60ce21f7401d1ef0
joaquinbraicovich <joaquintbraicovich@gmail.com>
2023-10-16 20:48:43 -0300

    header y footer

--- client/src/bookingApp/components/footer/Footer.jsx
+++ client/src/bookingApp/components/footer/Footer.jsx
@@ -1,3 +1,75 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className='flex flex-col items-center border-t bg-stone-200'>
+      <div className='flex flex-col p-4 gap-4 md:flex-row items-center w-full justify-evenly lg:w-2/3'>
+        <div className='flex flex-col items-center'>
+          <a href='/'>
+            <img className='w-50 h-40' src='/src/assets/bike.png' alt='' />
+          </a>
+          <h2 className='font-bold'>BikeMeNow</h2>
+        </div>
+
+        <div className='flex flex-row justify-evenly gap-8 font-bold text-slate-800 lg:text-xl '>
+          <nav>
+            <ul>
+              <Link to='/'>Home</Link>
+            </ul>
+            <ul>
+              <Link to='/'>Categories</Link>
+            </ul>
+            <ul>
+              <Link to='/'>Products</Link>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <Link to='/'>About Us</Link>
+            </ul>
+            <ul>
+              <Link to='/'>Community</Link>
+            </ul>
+            <ul>
+              <Link to='/'>Privacy Policy</Link>
+            </ul>
+          </nav>
+        </div>
+
+        <div className='flex gap-6 p-4 justify-center'>
+          <a href='https://www.facebook.com/' target='_blank'>
+            <img
+              className='w-8 h-8'
+              src='/src/assets/icon-facebook.svg'
+              alt=''
+            />
+          </a>
+          <a href='https://www.instagram.com/' target='_blank'>
+            <img
+              className='w-8 h-8 '
+              src='/src/assets/icon-instagram.svg'
+              alt=''
+            />
+          </a>
+          <a href='https://www.youtube.com/' target='_blank'>
+            <img
+              className='w-8 h-8 '
+              src='/src/assets/icon-youtube.svg'
+              alt=''
+            />
+          </a>
+          <a href='https://www.tiktok.com/' target='_blank'>
+            <img
+              className='w-8 h-8 '
+              src='/src/assets/icon-tiktok.svg'
+              alt=''
+            />
+          </a>
+        </div>
+      </div>
+      <div className='flex items-center justify-center bg-slate-900 w-full  text-stone-200 text-xs text-center h-10  font-bold  md:text-sm'>
+        © Copyright 2023 BikeMeNow. All Rights Reserved
+      </div>
+    </footer>
+  );
 };
--- client/src/bookingApp/components/header/Header.jsx
+++ client/src/bookingApp/components/header/Header.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return <div>Header</div>;
+  return (
+    <header className='pr-8 pl-8 pt-4 pb-4 flex items-center justify-around border-b shadow-lg top-0 w-full sticky bg-stone-200'>
+      <div>
+        <a href='/'>
+          <img className='w-30 h-20 ' src='/src/assets/bike.png' alt='' />
+        </a>
+        <h2 className='font-bold'>BikeMeNow</h2>
+      </div>
+
+      <div className='visibility: hidden lg:flex gap-5 font-bold text-xl'>
+        <div>
+          <Link to='/'>Bikes</Link>
+        </div>
+        <div>
+          <Link to='/'>Productos</Link>
+        </div>
+        <div>
+          <Link to='/'>Categorias</Link>
+        </div>
+        <div>
+          <Link to='/'>Ayuda</Link>
+        </div>
+      </div>
+
+      <div className='space-x-4 flex items-center justify-center '>
+        <button
+          className='middle none center mr-3 rounded-lg border border-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-blue-500 transition-all hover:opacity-75 focus:ring focus:ring-blue-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none '
+          data-ripple-dark='true'
+        >
+          Login
+        </button>
+        <button
+          className='middle none center mr-3 rounded-lg border border-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-blue-500 transition-all hover:opacity-75 focus:ring focus:ring-blue-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none  '
+          data-ripple-dark='true'
+        >
+          Register
+        </button>
+      </div>
+    </header>
+  );
 };
