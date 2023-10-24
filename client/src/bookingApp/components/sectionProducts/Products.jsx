f93f03b7b38bdcbb1bac790625d55ff034be2009
Germiquere <german150703.miquere@gmail.com>
2023-10-23 23:20:44 -0300

    merge exitoso

--- client/src/bookingApp/components/sectionProducts/Products.jsx
+++ client/src/bookingApp/components/sectionProducts/Products.jsx
@@ -3,12 +3,14 @@ import Section from "../Section";
 import GridProducts from "./GridProducts";
 
 export const Products = () => {
-  return (
-    <Section>
-      <div className='max-w-[1200px] mx-auto'>
-        <h2 className='text-lg sm:text-2xl font-semibold pb-2'>Recomendados</h2>
-        <GridProducts />
-      </div>
-    </Section>
-  );
+    return (
+        <Section>
+            <div className="max-w-[1200px] mx-auto">
+                <h2 className="text-lg sm:text-2xl font-semibold pb-2">
+                    Productos destacados
+                </h2>
+                <GridProducts />
+            </div>
+        </Section>
+    );
 };
