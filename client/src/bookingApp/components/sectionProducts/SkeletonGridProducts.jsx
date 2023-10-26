d3c09e7d0cbfc3fe54d77fc6e575b45d5cf02ccb
joaquinbraicovich <joaquintbraicovich@gmail.com>
2023-10-25 22:23:15 -0300

    productos skeleton

--- client/src/bookingApp/components/sectionProducts/SkeletonGridProducts.jsx
+++ client/src/bookingApp/components/sectionProducts/SkeletonGridProducts.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import Section from "../Section";
 
 export const SkeletonGridProducts = () => {
-  return <Section></Section>;
+  return (
+    <div
+      className={` mt-8 mb-8 rounded-xl shadow-md 
+        `}
+    >
+      <div className='bg-gray-200'>
+        <img className='rounded-xl  w-full h-48 object-contain' />
+      </div>
+      <p className='pl-4 pt-4 bg-gray-100'></p>
+      <p className='p-4 bg-gray-100 '></p>
+    </div>
+  );
 };
