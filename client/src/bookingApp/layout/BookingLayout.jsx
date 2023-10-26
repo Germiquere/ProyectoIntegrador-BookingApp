6bbf1206794df9d61ae4b0ca3fbf9df9e3a5c3dd
Germiquere <german150703.miquere@gmail.com>
2023-10-26 11:32:48 -0300

    sacando el modal previsorio

--- client/src/bookingApp/layout/BookingLayout.jsx
+++ client/src/bookingApp/layout/BookingLayout.jsx
@@ -4,16 +4,12 @@ import { Header } from "../components/header/Header";
 import { Footer } from "../components/footer/Footer";
 import { ResponsiveCalendarAndSearch } from "../components/sectionCalendarAndSearch/ResponsiveCalendarAndSearch";
 import { CalendarAndSearchContext } from "../../context/CalendarSearchContext";
-import { CreateProductModal } from "../components/admin/CreateProductModal";
-import { useBikesContext } from "../../context/BikesContext";
 
 export const BookingLayout = () => {
     const { openCalendarAndSearch } = useContext(CalendarAndSearchContext);
-    const { openNewProductModal } = useBikesContext();
     return (
         <>
             {openCalendarAndSearch && <ResponsiveCalendarAndSearch />}
-            {openNewProductModal && <CreateProductModal />}
             {/* <ResponsiveCalendarAndSearch /> */}
             <Header />
             <main className=" md:min-h-[calc(100vh-228px)] ">
