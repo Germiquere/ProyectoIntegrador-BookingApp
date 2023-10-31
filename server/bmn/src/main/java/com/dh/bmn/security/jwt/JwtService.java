3d0de940e6d60edb9495eab09e9cb5afcf94f303
cucuarce <cucuarce@gmail.com>
2023-10-31 00:10:51 -0300

    Se agregó anotación en JwtService

--- server/bmn/src/main/java/com/dh/bmn/security/jwt/JwtService.java
+++ server/bmn/src/main/java/com/dh/bmn/security/jwt/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.security.Key;
 import java.util.*;
 import java.util.function.Function;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Service
 public class JwtService {
