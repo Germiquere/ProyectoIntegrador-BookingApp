e5d4d71de0abf3cb225b0d6fbcdae63263d0dbf3
LucianaSalinardi <lu.salinardi@live.com.ar>
2023-11-05 04:05:26 -0300

    Tests clase CaracteristicaBicicletaService

--- server/bmn/src/main/java/com/dh/bmn/services/impl/CaracteristicaBicicletaService.java
+++ server/bmn/src/main/java/com/dh/bmn/services/impl/CaracteristicaBicicletaService.java
@@ -12,7 +12,6 @@ import com.dh.bmn.util.MapperClass;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,7 +23,7 @@ public class CaracteristicaBicicletaService implements IService<CaracteristicaBi
 
     private static final ObjectMapper objectMapper = MapperClass.objectMapper();
 
-    public CaracteristicaBicicletaService(ICaracteristicaBicicletaRepository caracteristicaRepository, ICaracteristicaBicicletaRepository caracteristicaBicicletaRepository) {
+    public CaracteristicaBicicletaService(ICaracteristicaBicicletaRepository caracteristicaBicicletaRepository) {
         this.caracteristicaBicicletaRepository = caracteristicaBicicletaRepository;
     }
 
--- server/bmn/src/test/java/com/dh/bmn/unit/CategoriaBicicletaServiceTest.java
+++ server/bmn/src/test/java/com/dh/bmn/unit/CategoriaBicicletaServiceTest.java
@@ -19,13 +19,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
