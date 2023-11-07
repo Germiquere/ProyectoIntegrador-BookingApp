e88f24464ff2e8a0de07952f3d91509af3243d0d
joaquin-braicovich <joaquintbraicovich@gmail.com>
2023-11-06 22:04:06 -0300

    desplegable

--- client/src/bookingApp/components/header/Header.jsx
+++ client/src/bookingApp/components/header/Header.jsx
@@ -17,7 +17,7 @@ export const Header = ({ user }) => {
           <Link className='w-10 h-10 bg-primary rounded-full flex items-center justify-center text-white text-md hover:bg-secondary mr-2'>
             {user.name.charAt(0).toUpperCase()}
           </Link>
-          <Link className='text-black mr-2'>{user.name}</Link>
+          <Link className='text-black mr-2 font-semibold'>{user.name}</Link>
           <div className='cursor-pointer' onClick={toggleDropdown}>
             <IoIosArrowDown />
           </div>
@@ -48,10 +48,24 @@ export const Header = ({ user }) => {
       </div>
 
       {isDropdownOpen && (
-        <div className='realitve top-full left-0 bg-white max-w-[1200px] mx-auto h-conatin pb-4 pl-4 flex items-end flex-col'>
-          <div>
+        <div
+          className={`absolute top-[55px] right-[5px] bg-white border border-gray-300 p-2 rounded-lg shadow-lg transition-opacity  xl:right-[355px] ${
+            isDropdownOpen ? 'opacity-100' : 'opacity-0'
+          }duration-300 ease-in-out`}
+        >
+          <div className='flex flex-col items-center'>
+            <div className='flex items-center'>
+              <p className='w-10 h-10 bg-primary rounded-full flex items-center justify-center text-white text-md  mr-2'>
+                {user.name.charAt(0).toUpperCase()}
+              </p>
+              <p className='text-black mr-2 font-bold'>
+                {user.name} {user.apellido}
+                <p className='font-semibold'>{user.email}</p>
+              </p>
+            </div>
+
             <button
-              className='sm:py-3 sm:px-6 middle none center  rounded-full border border-primary py-2 px-4 font-sans text-xs font-bold uppercase text-primary transition-all hover:opacity-75 focus:ring focus:ring-tertiary active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-4'
+              className=' middle none center  rounded-full border border-primary py-3 px-6 font-sans text-xs font-bold uppercase text-primary transition-all hover:opacity-75 focus:ring focus:ring-tertiary active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mt-4'
               data-ripple-dark='true'
             >
               Cerrar sesión
--- client/src/bookingApp/layout/BookingLayout.jsx
+++ client/src/bookingApp/layout/BookingLayout.jsx
@@ -1,22 +1,29 @@
-import React, { useContext } from "react";
-import { Outlet } from "react-router-dom";
-import { Header } from "../components/header/Header";
-import { Footer } from "../components/footer/Footer";
-import { ResponsiveCalendarAndSearch } from "../components/sectionCalendarAndSearch/ResponsiveCalendarAndSearch";
-import { CalendarAndSearchContext } from "../../context/CalendarSearchContext";
+import React, { useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components/header/Header';
+import { Footer } from '../components/footer/Footer';
+import { ResponsiveCalendarAndSearch } from '../components/sectionCalendarAndSearch/ResponsiveCalendarAndSearch';
+import { CalendarAndSearchContext } from '../../context/CalendarSearchContext';
 
 export const BookingLayout = () => {
-    const { openCalendarAndSearch } = useContext(CalendarAndSearchContext);
-    return (
-        <>
-            {openCalendarAndSearch && <ResponsiveCalendarAndSearch />}
-            {/* <ResponsiveCalendarAndSearch /> */}
-            <Header />
-            <main className="min-h-[calc(100vh-186px)] md:min-h-[calc(100vh-148px)] ">
-                <Outlet />
-                {/* <IconPicker /> */}
-            </main>
-            <Footer />
-        </>
-    );
+  const { openCalendarAndSearch } = useContext(CalendarAndSearchContext);
+
+  const user = {
+    name: 'Joaquin',
+    apellido: 'Braicovich',
+    email: 'joquintbraicovich@gmail.com',
+  };
+
+  return (
+    <>
+      {openCalendarAndSearch && <ResponsiveCalendarAndSearch />}
+      {/* <ResponsiveCalendarAndSearch /> */}
+      <Header user={user} />
+      <main className='min-h-[calc(100vh-186px)] md:min-h-[calc(100vh-148px)] '>
+        <Outlet />
+        {/* <IconPicker /> */}
+      </main>
+      <Footer />
+    </>
+  );
 };
