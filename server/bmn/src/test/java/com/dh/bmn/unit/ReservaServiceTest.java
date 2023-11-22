1c29c98205b62f5b89575d3e89523894a1c5a16b
LucianaSalinardi <lu.salinardi@live.com.ar>
2023-11-22 00:32:51 -0300

    Se agrega dockerfile y modificacion en application.properties

--- server/bmn/src/test/java/com/dh/bmn/unit/ReservaServiceTest.java
+++ server/bmn/src/test/java/com/dh/bmn/unit/ReservaServiceTest.java
@@ -493,7 +493,7 @@ public class ReservaServiceTest {
         BicicletaRequestDto bicicletaRequestDto =
                 new BicicletaRequestDto(1L, "Bike", "Ideal para montaña", 34567, categoriaRequestList, imagenes, caracteristicaRequestList,politicaBicicletaRequestList);
         UsuarioRequestDto usuarioRequestDto = new UsuarioRequestDto(1L, "Juan", "Perez", "juan.perez@gmail.com");
-        ReservaRequestDto reservaRequestDto = new ReservaRequestDto(1L, usuarioRequestDto, bicicletaRequestDto, LocalDate.of(2023, 11, 19), LocalDate.of(2023, 11, 10));
+        ReservaRequestDto reservaRequestDto = new ReservaRequestDto(1L, usuarioRequestDto, bicicletaRequestDto, LocalDate.of(2023, 11, 21), LocalDate.of(2023, 11, 20));
         IllegalDateException exception = Assertions.assertThrows(IllegalDateException .class, () -> {
             reservaService.crear(reservaRequestDto);
         });
